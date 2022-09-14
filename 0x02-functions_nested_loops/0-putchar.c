ec6988efae655c4846855841517bb69e4c1f9391
Atafe123 <enaatafe123@gmail.com>
2022-09-14 15:01:01 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,18 +5,18 @@
  * Return: always 0
  */
 
-int main(void)
-(         
-        _putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('/n')/
-	
-	return (0);
-)
+int main(void)n
+(
+         _putchar('_');
+	 _putchar('p');
+	 _putchar('u');
+         _putchar('t');
+	 _putchar('c');
+	 _putchar('h');
+	 _putchar('a');
+	 _putchar('r');
+	 _putchar('/n');
 
+	 return (0);
+
+)	 
