10252fea143a3139d260447df8e98e3ee1be0ed2
Atafe123 <enaatafe123@gmail.com>
2022-09-15 14:26:53 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-
+#include "main.h"
 
 /**
  *
@@ -13,24 +13,18 @@
  *     */
 
 int main(void)
-{
-        _putchar ('_');
 
-	_putchar ('p');
+{ 
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 
-	_putchar ('u');
+	_putchar('/n');
 
-	_putchar ('t');
-
-	_putchar ('c');
-
-	_putchar ('h');
-
-	_putchar ('a');
-
-	_putchar ('r');
-
-        _putchar('\n');
-
-        return (0);
-}	
+        return(0);
+}
